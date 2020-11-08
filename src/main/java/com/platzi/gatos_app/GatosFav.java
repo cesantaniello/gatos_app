918244da20dcf9b9dca5be3a59496ae8256f8c26
Carlos <carlos.e.santaniello@gmail.com>
2020-11-08 23:07:34 +0100

    Listar favoritos

--- src/main/java/com/platzi/gatos_app/GatosFav.java
+++ src/main/java/com/platzi/gatos_app/GatosFav.java
@@ -10,5 +10,48 @@ package com.platzi.gatos_app;
  * @author carlos
  */
 public class GatosFav {
+    String id;
+    String image_id;
+    String apikey = "bafe2021-67c5-4b55-94bc-59a7f88e4e97";
+    Imagex image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(String image_id) {
+        this.image_id = image_id;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public Imagex getImage() {
+        return image;
+    }
+
+    public void setImage(Imagex image) {
+        this.image = image;
+    }
+
+    class image {
+
+        public image() {
+        }
+    }
+    
     
 }
--- src/main/java/com/platzi/gatos_app/Imagex.java
+++ src/main/java/com/platzi/gatos_app/Imagex.java
@@ -10,5 +10,24 @@ package com.platzi.gatos_app;
  * @author carlos
  */
 public class Imagex {
+    String id;
+    String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
     
 }
